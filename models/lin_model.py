5086462b8acb4e9f26e364ddd23e6aa854770cf3
Gabriel Gavrilas <gabriel.gavrilas@gmail.com>
2021-12-28 14:58:35 +0100

    small changes to lin_model

--- models/lin_model.py
+++ models/lin_model.py
@@ -3,16 +3,15 @@ from torch.nn import Transformer
 import numpy as np
 
 class CustomModule(torch.nn.Module):
-    def __init__(self, time_step, lead_time):
+    def __init__(self, device):
         super(CustomModule, self).__init__()
-        self.time_step = time_step
-        self.lead_time = lead_time
         self.input_dim = 2048
         self.output_dim = 2048
         self.linear = torch.nn.Linear(self.input_dim, self.output_dim)
+        self.device = device
 
     def forward(self, x, y):
-        x = x[:,-1]
+        x = x[:,-1].to(self.device)
         x = x.reshape(x.shape[0], self.input_dim)
         if not self.training:
             with torch.no_grad():
